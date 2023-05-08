d105c25d9ada60a892f29f535f304772790db6b1
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-05-09 04:30:28 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -11,7 +11,7 @@ async def start_handler(client: Client, msg: types.Message):
     username = (
         f'@{msg.from_user.username}'
         if msg.from_user.username
-        else '@chatjomblohalu_bot'
+        else '@OwnNeko'
     )
     mention = msg.from_user.mention
     await msg.reply_text(
