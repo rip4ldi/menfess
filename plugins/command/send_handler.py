026cda99e30a91071194021ec06b3009a206f4d6
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-05-12 20:49:56 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -30,6 +30,8 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
             picture = config.pic_talentgirl
         if user.status == 'owner':
             picture = config.pic_owner
+       if user.status == 'admin'
+            picture = config.pic_admin
 
             
 
