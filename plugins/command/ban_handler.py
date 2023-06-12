99227b054acf871e8c034c3386e3f2baadf5e2a6
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-06-12 09:12:02 +0700

    Update ban_handler.py

--- plugins/command/ban_handler.py
+++ plugins/command/ban_handler.py
@@ -43,7 +43,7 @@ async def ban_handler(client: Client, msg: types.Message):
     await db.banned_user(int(target), client.id_bot, alasan)
     
     # Send ban notification to the main channel
-    await client.send_message(config.channel_main, f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil dibanned</i>\n└Dibanned oleh : <a href='tg://openmessage?user_id={str(config.id_admin)}'>Admin</a>\n\nAlasan: {str(alasan)}\n\n{update}", parse_mode=enums.ParseMode.HTML)
+    await client.send_message(config.channel_1, f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil dibanned</i>\n└Dibanned oleh : <a href='tg://openmessage?user_id={str(config.id_admin)}'>Admin</a>\n\nAlasan: {str(alasan)}\n\n{update}", parse_mode=enums.ParseMode.HTML)
     
     return await msg.reply_text(
         text=f"<a href='tg://openmessage?user_id={str(target)}'>User</a> <i>berhasil dibanned</i>\n└Dibanned oleh : <a href='tg://openmessage?user_id={str(config.id_admin)}'>Admin</a>\n\nAlasan: {str(alasan)}\n\n{update}",
