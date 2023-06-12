75f179080e818f921d48192e3a7b39567f6458f1
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-06-12 11:54:25 +0700

    Update __init__.py

--- plugins/command/__init__.py
+++ plugins/command/__init__.py
@@ -4,6 +4,7 @@ from .send_handler import *
 from .ban_handler import *
 from .bot_handler import *
 from .talent_handler import *
+from .check_handler import *
 from .daddy_sugar import *
 from .moans_girl import *
 from .moans_boy import *
