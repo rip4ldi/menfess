95cf73d57d400add22638721555ec5ded2c0130e
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-06-14 19:17:34 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -38,6 +38,9 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
             pictur = config.pic_bfrent
         elif user.status == 'moans boy':
             picture = config.pic_moansboy
+        if user.status == 'member':
+            if user.user_id == '5505030378':
+                picture = config.pic_rekberboy
 
             
             
