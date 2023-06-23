d84c33942d9a9a0a423f02c297d8d99919aceaea
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-06-24 00:31:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ db_name = os.environ.get("DB_NAME", "menfess")
 
 channel_1 = int(os.environ.get("CHANNEL_1", "-1001884106616"))
 channel_2 = int(os.environ.get("CHANNEL_2", "-1001973439933"))
+channel_3 = int(os.environ.get("CHANNEL_3", "-1001884106616"))
 channel_log = int(os.environ.get("CHANNEL_LOG", "-1001941178911"))
 # =========================================================== #
 
