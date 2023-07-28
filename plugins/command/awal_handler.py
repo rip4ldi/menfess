5e71a92363713035f360554bfe16fac9e758bb2b
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-29 01:35:04 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -86,17 +86,47 @@ async def list_admin_handler(helper: Helper, id_bot: int):
     await helper.message.reply_text(pesan, True, enums.ParseMode.HTML)
 
 
-async def list_ban_handler(helper: Helper, id_bot: int):
+async def list_ban_handler(helper: Helper, id_bot: int, page: int = 1):
     db = Database(helper.user_id).get_data_bot(id_bot)
-    if len(db.ban) == 0:
+    banned_users = db.ban
+
+    if not banned_users:
         return await helper.message.reply_text('<i>Tidak ada user dibanned saat ini</i>', True, enums.ParseMode.HTML)
-    pesan = "<b>Daftar banned</b>\n"
-    for ind, i in enumerate(db.ban, start=1):
-        pesan += (
-            f"• ID: {str(i)} | <a href='tg://openmessage?user_id={str(i)}'>( {str(ind)}"
-            + " )</a>\n"
-        )
-    await helper.message.reply_text(pesan, True, enums.ParseMode.HTML)
+
+    # Jumlah daftar banned yang ingin ditampilkan dalam satu pesan
+    banned_per_page = 5
+
+    # Hitung total halaman yang dibutuhkan untuk menampilkan semua banned user
+    total_pages = (len(banned_users) - 1) // banned_per_page + 1
+
+    # Pastikan nomor halaman valid (di antara 1 dan total_pages)
+    page = max(1, min(page, total_pages))
+
+    # Hitung indeks awal dan akhir daftar banned untuk halaman yang ditentukan
+    start_idx = (page - 1) * banned_per_page
+    end_idx = min(start_idx + banned_per_page, len(banned_users))
+
+    # Siapkan pesan awal untuk daftar banned
+    pesan = "<b>Daftar banned (Halaman {}/{})</b>\n".format(page, total_pages)
+    pesan += "<pre>"
+
+    # Tambahkan daftar banned untuk halaman tertentu ke dalam pesan
+    for ind, i in enumerate(banned_users[start_idx:end_idx], start=start_idx + 1):
+        pesan += f"• ID: {str(i)} | <a href='tg://openmessage?user_id={str(i)}'>( {str(ind)}"
+        pesan += " )</a>\n"
+
+    pesan += "</pre>"
+
+    # Buat tombol untuk 'Next' dan 'Back'
+    keyboard = []
+    if page > 1:
+        keyboard.append([InlineKeyboardButton("Back", callback_data=f"ban_back_{id_bot}_{page-1}")])
+    if page < total_pages:
+        keyboard.append([InlineKeyboardButton("Next", callback_data=f"ban_next_{id_bot}_{page+1}")])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await helper.message.reply_text(pesan, True, enums.ParseMode.HTML, reply_markup=reply_markup)
 
 
 
