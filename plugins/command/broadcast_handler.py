8d94f0ba7aa6a02c9b0f21bfd4dfccef9fd3eea5
NekoLocal <132919248+nekolocal@users.noreply.github.com>
2023-07-31 04:12:06 +0700

    Update broadcast_handler.py

--- plugins/command/broadcast_handler.py
+++ plugins/command/broadcast_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+
 from pyrogram import Client
 from pyrogram.types import (
     Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
@@ -13,11 +14,12 @@ async def broadcast_handler(client: Client, msg: Message):
         await msg.reply('Harap reply sebuah pesan', True)
     else:
         anu = msg.reply_to_message
-        anu = await anu.copy(msg.chat.id, reply_to_message_id=anu.id)
+        anu = await anu.copy(msg.chat.id, reply_to_message_id=anu.message_id)
         markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton('Ya', 'ya_confirm'), InlineKeyboardButton('Tidak', 'tidak_confirm')]
+            [InlineKeyboardButton('Ya', 'ya_confirm'), InlineKeyboardButton('Tidak', 'tidak_confirm')],
+            [InlineKeyboardButton('Ya dan Pin', 'ya_pin'), InlineKeyboardButton('Tidak dan Pin', 'tidak_pin')]
         ])
-        await anu.reply('Apakah kamu akan mengirimkan pesan broadcast?', True, reply_markup=markup)
+        await anu.reply('apakah kamu akan mengirimkan pesan broadcast ?', True, reply_markup=markup)
 
 async def broadcast_ya(client: Client, query: CallbackQuery):
     msg = query.message
@@ -26,6 +28,7 @@ async def broadcast_ya(client: Client, query: CallbackQuery):
         await query.answer('Pesan tidak ditemukan', True)
         await query.message.delete()
         return
+
     message = msg.reply_to_message
     user_ids = db.get_pelanggan().id_pelanggan
 
@@ -33,7 +36,9 @@ async def broadcast_ya(client: Client, query: CallbackQuery):
     dihapus = 0
     blokir = 0
     gagal = 0
+
     await msg.edit('Broadcast sedang berlangsung, tunggu sebentar', reply_markup=None)
+
     for user_id in user_ids:
         try:
             await message.copy(user_id)
@@ -49,8 +54,9 @@ async def broadcast_ya(client: Client, query: CallbackQuery):
         except InputUserDeactivated:
             dihapus += 1
             await db.hapus_pelanggan(user_id)
-    text = f"""<b>Broadcast selesai</b>
 
+    text = f"""<b>Broadcast selesai</b>
+    
 Jumlah pengguna: {len(user_ids)}
 Berhasil terkirim: {str(berhasil)}
 Pengguna diblokir: {str(blokir)}
@@ -60,17 +66,6 @@ Gagal terkirim: {str(gagal)}"""
     await msg.reply(text)
     await msg.delete()
     await message.delete()
-
-async def broadcast_pin_handler(client: Client, msg: Message):
-    if msg.reply_to_message is None:
-        await msg.reply('Harap reply sebuah pesan untuk disematkan', True)
-    else:
-        anu = msg.reply_to_message
-        anu = await anu.copy(msg.chat.id, reply_to_message_id=anu.id)
-        markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton('Ya', 'ya_pin'), InlineKeyboardButton('Tidak', 'tidak_pin')]
-        ])
-        await anu.reply('Apakah kamu akan mengirimkan pesan broadcast dan menyematkannya?', True, reply_markup=markup)
 
 async def broadcast_pin_ya(client: Client, query: CallbackQuery):
     msg = query.message
@@ -79,6 +74,7 @@ async def broadcast_pin_ya(client: Client, query: CallbackQuery):
         await query.answer('Pesan tidak ditemukan', True)
         await query.message.delete()
         return
+
     message = msg.reply_to_message
     user_ids = db.get_pelanggan().id_pelanggan
 
@@ -86,16 +82,22 @@ async def broadcast_pin_ya(client: Client, query: CallbackQuery):
     dihapus = 0
     blokir = 0
     gagal = 0
+    sent_message = None
+
+    try:
+        sent_message = await message.copy(msg.chat.id)
+    except:
+        pass
+
     await msg.edit('Broadcast sedang berlangsung, tunggu sebentar', reply_markup=None)
+
     for user_id in user_ids:
         try:
-            sent_message = await message.copy(user_id)
-            await client.pin_chat_message(sent_message.chat.id, sent_message.message_id)
+            await sent_message.copy(user_id)
             berhasil += 1
         except FloodWait as e:
             await asyncio.sleep(e.x)
-            sent_message = await message.copy(user_id)
-            await client.pin_chat_message(sent_message.chat.id, sent_message.message_id)
+            await sent_message.copy(user_id)
             berhasil += 1
         except UserIsBlocked:
             blokir += 1
@@ -104,25 +106,20 @@ async def broadcast_pin_ya(client: Client, query: CallbackQuery):
         except InputUserDeactivated:
             dihapus += 1
             await db.hapus_pelanggan(user_id)
-    text = f"""<b>Broadcast selesai</b>
 
+    text = f"""<b>Broadcast selesai</b>
+    
 Jumlah pengguna: {len(user_ids)}
 Berhasil terkirim: {str(berhasil)}
-Pesan berhasil disematkan: {str(berhasil)}
 Pengguna diblokir: {str(blokir)}
 Akun yang dihapus: {str(dihapus)} (<i>Telah dihapus dari database</i>)
 Gagal terkirim: {str(gagal)}"""
 
     await msg.reply(text)
+    try:
+        await client.pin_chat_message(sent_message.chat.id, sent_message.message_id)
+    except Exception as e:
+        print(f"Error saat pin message: {e}")
+    
     await msg.delete()
     await message.delete()
-
-async def close_cbb(client: Client, query: CallbackQuery):
-    try:
-        await query.message.reply_to_message.delete()
-    except:
-        pass
-    try:
-        await query.message.delete()
-    except:
-        pass
